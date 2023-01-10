a7d6922058cd73e6b3e48d95161ee8956a8d4044
sergey-077 <113626341+sergey-077@users.noreply.github.com>
2023-01-11 00:09:56 +0300

    Update Task71.java

--- Task71.java
+++ Task71.java
@@ -3,13 +3,15 @@ class Solution {
         Stack<String> s = new Stack<>();
         String res = "";
         String[] p = path.split("/");
-        for(int i = 0; i < p.length; i++){
-            if(!s.isEmpty() && p[i].equals("..")) s.pop();
-            else if(!p[i].equals("") && !p[i].equals(".") && !p[i].equals(".."))
+        for(int i = 0; i < p.length; i++) {
+            if(!s.isEmpty() && p[i].equals("..")) { 
+                s.pop();
+            }else if(!p[i].equals("") && !p[i].equals(".") && !p[i].equals("..")) {
                 s.push(p[i]);
+            }
         }
         if(s.isEmpty()) return "/";
-        while(!s.isEmpty()){
+        while(!s.isEmpty()) {
             res = "/" + s.pop() + res;
         }
         return res;
